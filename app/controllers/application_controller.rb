8091be6d907ebdbe6ae7477f6b5e8e96a13997ce
Zhenya Telyukov <telyukov@gmail.com>
2017-06-03 17:08:01 +0300

    protection is on

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   include ZT
-#  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
   before_action :set_locale
 end
